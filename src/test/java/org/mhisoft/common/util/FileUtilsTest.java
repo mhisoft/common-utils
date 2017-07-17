491647fa91e061d46a4854212223cc7719f8e3c2
mhisoft <mhisoft@github.com>
2017-07-16 21:21:57 -0400

    remove attachment working in progress

--- src/test/java/org/mhisoft/common/util/FileUtilsTest.java
+++ src/test/java/org/mhisoft/common/util/FileUtilsTest.java
@@ -9,7 +9,7 @@
  *  * not use this file except in compliance with the License.
  *  * You may obtain a copy of the License at
  *  *
- *  *    http://www.apache.org/licenses/LICENSE-2.0
+ *  *    http:/www.apache.org/licenses/LICENSE-2.0
  *  *
  *  * Unless required by applicable law or agreed to in writing,
  *  * software distributed under the License is distributed on an
@@ -37,25 +37,25 @@ public class FileUtilsTest {
 
 	@Test
 	public void getFileNameWithoutPath() {
-		String s = "S:\\projects\\mhisoft\\evault-app\\dist\\test.docx";
+		String s = "S:/projects/mhisoft/evault-app/dist/test.docx";
 		Assert.assertEquals("test.docx", FileUtils.getFileNameWithoutPath(s));
 
-		String s2 = "S:\\projects\\mhisoft\\evault-app\\dist";
+		String s2 = "S:/projects/mhisoft/evault-app/dist";
 		Assert.assertEquals(null, FileUtils.getFileNameWithoutPath(s2));
 
-		String s3 = "S:\\projects\\mhisoft\\evault-app\\";
+		String s3 = "S:/projects/mhisoft/evault-app/";
 		Assert.assertNull(FileUtils.getFileNameWithoutPath(s3));
 	}
 
 	@Test
 	public void getFileDir() {
-		String s = "S:\\projects\\mhisoft\\evault-app\\dist\\test.docx";
-		Assert.assertEquals("S:\\projects\\mhisoft\\evault-app\\dist", FileUtils.gerFileDir(s));
+		String s = "S:/projects/mhisoft/evault-app/dist/test.docx";
+		Assert.assertEquals("S:/projects/mhisoft/evault-app/dist", FileUtils.gerFileDir(s));
 
-		String s2 = "S:\\projects\\mhisoft\\evault-app\\dist";
-		Assert.assertEquals("S:\\projects\\mhisoft\\evault-app\\dist", FileUtils.gerFileDir(s2));
+		String s2 = "S:/projects/mhisoft/evault-app/dist";
+		Assert.assertEquals("S:/projects/mhisoft/evault-app/dist", FileUtils.gerFileDir(s2));
 
-		String s3 = "S:\\projects\\mhisoft\\evault-app\\";
-		Assert.assertEquals("S:\\projects\\mhisoft\\evault-app", FileUtils.gerFileDir(s3));
+		String s3 = "S:/projects/mhisoft/evault-app/";
+		Assert.assertEquals("S:/projects/mhisoft/evault-app", FileUtils.gerFileDir(s3));
 	}
 }
